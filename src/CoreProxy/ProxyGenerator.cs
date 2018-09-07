ee3734e12d55df9982487503547fc43aa0a6e934
costyuan <89783820@qq.com>
2018-09-07 19:46:12 +0800

    合并创建代理的表达式

--- src/CoreProxy/ProxyGenerator.cs
+++ src/CoreProxy/ProxyGenerator.cs
@@ -44,9 +44,26 @@ namespace CoreProxy
         /// <returns>代理实例</returns>
         public static TTarget Create<TTarget, TInterceptor>(params object[] parameters) where TInterceptor : IInterceptor
         {
-            var proxy = GetProxy(typeof(TTarget));
-            ((ProxyGenerator)proxy).CreateInstance(typeof(TInterceptor), parameters);
-            return (TTarget)proxy;
+            //var proxy = GetProxy(typeof(TTarget));
+            //((ProxyGenerator)proxy).CreateInstance(typeof(TInterceptor), parameters);
+            //return (TTarget)proxy;
+            var targetType = typeof(TTarget);
+            var interceptorType = typeof(TInterceptor);
+            //声明一个返回值变量
+            var variable = Expression.Variable(targetType);
+
+            var callexp = Expression.Call(typeof(DispatchProxy), nameof(DispatchProxy.Create), new[] { targetType, typeof(ProxyGenerator) });
+            var ctorParams = parameters.Select(x => x.GetType()).ToArray();
+            var paramsExp = parameters.Select(x => Expression.Constant(x));
+            var newExp = Expression.New(interceptorType.GetConstructor(ctorParams), paramsExp);
+            var interceptorProperty = Expression.Property(Expression.Convert(variable,typeof(ProxyGenerator)), nameof(interceptor));
+
+            var assign1 = Expression.Assign(variable, callexp);//赋值操作
+            var assign2 = Expression.Assign(interceptorProperty, newExp);//给接口复制
+
+            //构造代码块 
+            var block = Expression.Block(new[] { variable }, assign1,assign2, variable);
+            return Expression.Lambda<Func<TTarget>>(block).Compile()();//建议缓存
         }
 
         private static object GetProxy(Type targetType)
