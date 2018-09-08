7ed6d2e8a4ea53a2f4f032829b52772de904c8f1
costyuan <89783820@qq.com>
2018-09-08 09:36:07 +0800

    增加调用示例

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -16,6 +16,9 @@ namespace ConsoleApp
 
             var poxy3 = ProxyGenerator.Create<targetInterface, SampleProxy>("coreproxy3");
             poxy3.Write("here was invoked"); //---> "here was invoked by coreproxy3"
+            
+            var poxy4 = ProxyGenerator.Create<targetInterface>(new SampleProxy("coreproxy4"));
+            poxy4.Write("here was invoked"); //---> "here was invoked by coreproxy4"
         }
     }
 
